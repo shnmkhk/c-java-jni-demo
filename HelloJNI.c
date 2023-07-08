0c9d3f3e28b0a85e7f4fff53ab803cc19b321929
Shanmukha Katuri <shanmukha.k@gmail.com>
2023-07-08 05:40:48 +0000

    shnmkhk: fixed linux build script

--- HelloJNI.c
+++ HelloJNI.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "HelloJNI.h"
 
-JNIEXPORT void JNICALL Java_HelloJNI_sayHello(JNIEnv *, jobject)
+JNIEXPORT void JNICALL Java_HelloJNI_sayHello(JNIEnv *env, jobject jobj)
 {
     printf("Hello World from C Runtime!\n");
 }
--- build-n-run-linux.sh
+++ build-n-run-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Set JAVA_HOME to JDK 8 or later
 $JAVA_HOME/bin/javac -h . HelloJNI.java
-gcc -fPIC -I"$JAVA_HOME/include" -I "$JAVA_HOME/include/darwin" -shared -o libhello.so HelloJNI.c
+gcc -fPIC -I"$JAVA_HOME/include" -I "$JAVA_HOME/include/linux" -shared -o libhello.so HelloJNI.c
 $JAVA_HOME/bin/java -Djava.library.path=. HelloJNI
 # native-image -Djava.library.path=. HelloJNI
 # time ./hellojni
