5a011d29d45895b1b97aa1eeb8038dff0c98d5cb
Shanmukha Katuri <shanmukha.k@gmail.com>
2023-07-08 06:06:00 +0000

    Added log statements to build steps on build script

--- build-n-run-mac.sh
+++ build-n-run-mac.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+echo [INFO] Cleaning up the previous binaries
+rm *.class *.h *.dylib;
 # Set JAVA_HOME to JDK 8 or later
+echo [INFO] Compiling Java source files
 $JAVA_HOME/bin/javac -h . HelloJNI.java
+echo [INFO] Compiling native C/C++ source files
 gcc -I"$JAVA_HOME/include" -I "$JAVA_HOME/include/darwin" -dynamiclib -o libhello.dylib HelloJNI.c
+echo [INFO] Launching the program
 $JAVA_HOME/bin/java -Djava.library.path=. HelloJNI
 # native-image -Djava.library.path=. HelloJNI
 # time ./hellojni
