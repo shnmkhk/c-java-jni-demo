d8c5610ef1f86cba26acf904fcd21cd65dd89246
Shanmukha Katuri <shanmukha.k@gmail.com>
2023-07-08 06:03:44 +0000

    Added logs for the build process steps

--- build-n-run-linux.sh
+++ build-n-run-linux.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+echo [INFO] Cleaning up the previous binaries
+rm *.class *.h *.so;
 # Set JAVA_HOME to JDK 8 or later
+echo [INFO] Compiling Java source files
 $JAVA_HOME/bin/javac -h . HelloJNI.java
+echo [INFO] Compiling native C/C++ source files
 gcc -fPIC -I"$JAVA_HOME/include" -I "$JAVA_HOME/include/linux" -shared -o libhello.so HelloJNI.c
+echo [INFO] Launching the program
 $JAVA_HOME/bin/java -Djava.library.path=. HelloJNI
 # native-image -Djava.library.path=. HelloJNI
 # time ./hellojni
